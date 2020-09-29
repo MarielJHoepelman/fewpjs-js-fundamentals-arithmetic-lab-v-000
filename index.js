2f0c793d6d2654401428adc1d7419e7643e3af20
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-09-29 14:40:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 // Write your code here
 // Create a variable called multiply that will multiply the variables num1 and num2 whose results will equal "62".
-let num1 = 
-let num2;
+let num1 = 2;
+let num2 = 32;
 let multiply = num1 * num2;
